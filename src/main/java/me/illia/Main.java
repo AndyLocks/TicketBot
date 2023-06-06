069ad7072be8f48267b7343263fc0959e48f3091
Илья <112815007+AndyLocks@users.noreply.github.com>
2023-06-06 18:24:24 +0200

    aboba

--- src/main/java/me/illia/Main.java
+++ src/main/java/me/illia/Main.java
@@ -1,28 +1,42 @@
 package me.illia;
 
+import events.HelloEvent;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.SubscribeEvent;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.jetbrains.annotations.NotNull;
 
-import javax.security.auth.login.LoginException;
+import java.util.Collection;
+import java.util.Iterator;
+
 
 public class Main {
 
-    public static void main(String[] args) throws LoginException, InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
 
         System.out.println("pidor");
 
-        JDA bot = JDABuilder.createDefault("MTExNDk3NDYwNDc4NDU3ODU5MA.GlOfji.kKT1UO7gWdgBDSDZnRM5L2PTZwM-ek0mb8RZ3U")
-                .setActivity(Activity.playing("aboba"))
-                .build().awaitReady();
+        JDA bot = JDABuilder.create(
+                "MTExNDk3NDYwNDc4NDU3ODU5MA.GlOfji.kKT1UO7gWdgBDSDZnRM5L2PTZwM-ek0mb8RZ3U",
+                GatewayIntent.GUILD_MESSAGES,
+                GatewayIntent.AUTO_MODERATION_CONFIGURATION,
+                GatewayIntent.AUTO_MODERATION_EXECUTION,
+                GatewayIntent.DIRECT_MESSAGE_REACTIONS,
+                GatewayIntent.DIRECT_MESSAGE_TYPING,
+                GatewayIntent.DIRECT_MESSAGES,
+                GatewayIntent.GUILD_EMOJIS_AND_STICKERS,
+                GatewayIntent.GUILD_INVITES,
+                GatewayIntent.GUILD_MEMBERS,
+                GatewayIntent.GUILD_MESSAGE_REACTIONS,
+                GatewayIntent.GUILD_MESSAGE_TYPING,
+                GatewayIntent.GUILD_MODERATION,
+                GatewayIntent.GUILD_PRESENCES,
+                GatewayIntent.GUILD_VOICE_STATES,
+                GatewayIntent.MESSAGE_CONTENT,
+                GatewayIntent.SCHEDULED_EVENTS
+        ).build();
 
-    }
-
-    @SubscribeEvent
-    public void ohHeyAMessage(MessageReceivedEvent event)
-    {
-        System.out.println(event.getMessage().getContentDisplay());
+        bot.addEventListener(new HelloEvent());
     }
 }
