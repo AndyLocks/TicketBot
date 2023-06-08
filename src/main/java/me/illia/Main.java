b2ab3d9b0a2bccbe0044067bb73a80152763a51d
Илья <112815007+AndyLocks@users.noreply.github.com>
2023-06-08 14:15:25 +0200

    Na ja)

--- src/main/java/me/illia/Main.java
+++ src/main/java/me/illia/Main.java
@@ -7,9 +7,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
-import net.dv8tion.jda.api.utils.ChunkingFilter;
-import net.dv8tion.jda.api.utils.MemberCachePolicy;
-import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class Main {
 
